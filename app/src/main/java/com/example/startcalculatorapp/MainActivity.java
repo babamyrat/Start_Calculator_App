bdc3413900dd27a98232af3739e21853f463946b
tmstudent <tmstudentler@gmail.com>
2021-09-11 11:54:25 +0300

    step

--- app/src/main/java/com/example/startcalculatorapp/MainActivity.java
+++ app/src/main/java/com/example/startcalculatorapp/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.startcalculatorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +15,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://www.app.com/");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+
+                startActivity(intent);
+            }
+        });
+
     }
+
 
 
 
